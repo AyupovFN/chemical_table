cae8f14b2d81e9c6f8d10e313c087510fef23fd6
FAyupov <kppfsb@mail.com>
2020-02-01 23:37:44 +0300

    modified chemical table 2

--- chemical_table.rb
+++ chemical_table.rb
@@ -4,8 +4,8 @@ def result_print(user_choice)
   <<~HEREDOC
     Порядковый номер: #{@chemical_table[user_choice]["number"]}
     Название: #{@chemical_table[user_choice]["name"]}
-    Первооткрыватель: #{@chemical_table[user_choice]["year"]}
-    Плотность: #{@chemical_table[user_choice]["density"]}
+    Первооткрыватель: #{@chemical_table[user_choice]["discoverer"]} (#{@chemical_table[user_choice]["year"]} г.)
+    Плотность: #{@chemical_table[user_choice]["density"]} г/см³
   HEREDOC
 end
 
