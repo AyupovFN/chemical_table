ce866e51976fbff7d450f7b5698aa3704f5b20b5
FAyupov <kppfsb@mail.com>
2020-02-02 22:39:53 +0300

    modified chemical table add method capitalize

--- chemical_table.rb
+++ chemical_table.rb
@@ -13,12 +13,10 @@ file = File.read("#{__dir__}/chemical_table.json", encoding: 'UTF-8')
 @chemical_table = JSON.parse(file)
 # Выводим пользователю количество элементов
 puts "У нас всего элементов: #{@chemical_table.keys.size}"
-# Выводим пользователю сами элементы (ключи ассоциативного массива)
 puts "#{@chemical_table.keys.join(', ')}"
 # Спрашиваем у пользователя, какой элемент его интересует и записываем его
-# выбор в переменную element.
 puts "О каком элементе хотите узнать?"
-user_choice = gets.chomp
+p user_choice = gets.chomp.capitalize
 # Проверка — есть ли в хэше элемент, который спросил пользователь
 if @chemical_table.key?(user_choice)
   puts result_print(user_choice)
